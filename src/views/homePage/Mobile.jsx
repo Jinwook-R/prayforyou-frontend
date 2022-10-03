a4f647f4b9558713643d7b418c2e876beb381e2e
Joonyoung Choi <mydream757@gmail.com>
2022-10-03 13:28:00 +0900

    Fix : 배너 임시 삭제

--- src/views/homePage/Mobile.jsx
+++ src/views/homePage/Mobile.jsx
@@ -1,16 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Banner, Title, Search, DropDown } from "../../components";
-import { useSelector, useDispatch } from "react-redux";
+import { Title, Search, DropDown } from "../../components";
+import { useDispatch } from "react-redux";
 import { fetchAllBanners } from "../../redux/banner/bannerSlice";
 
-const bannerProps = {
-  width: "100%",
-  height: "100px",
-  marginBottom: "16px",
-};
-
 const Mobile = () => {
-  const banners = useSelector((store) => store.banner);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -24,13 +17,6 @@ const Mobile = () => {
 
   return (
     <div style={{ padding: "0 16px" }}>
-      <Banner
-        imgUrl={banners?.data?.typeA}
-        {...bannerProps}
-        height="50px"
-        marginBottom="100px"
-        marginTop="35px"
-      />
       <Title width="210px" />
       <div
         style={{ position: "relative" }}
@@ -56,22 +42,6 @@ const Mobile = () => {
           />
         )}
       </div>
-      <Banner
-        imgUrl={banners?.data?.typeB}
-        {...bannerProps}
-        marginTop="16px"
-        width="90%"
-        marginLeft="auto"
-        marginRight="auto"
-      />
-      <Banner
-        imgUrl={banners?.data?.typeC}
-        {...bannerProps}
-        width="90%"
-        marginLeft="auto"
-        marginRight="auto"
-      />
-      <Banner imgUrl={banners?.data?.typeA} {...bannerProps} height="50px" />
     </div>
   );
 };
